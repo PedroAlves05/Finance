c2303d239bfe735210adeac11f3df1ac2b50abab
PEDRO  HENRIQUE MINGOIA ALVES <0220482312016@fatectq.local>
2024-06-18 17:26:30 -0300

    Arrumando erros

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('estoque/', views.estoque, name="estoque"),
@@ -8,5 +9,6 @@ urlpatterns = [
     path('adicionar_vendas/', views.adicionar_vendas, name="adicionar_vendas"),
     path('adicionar_estoque/', views.adicionar_estoque, name="adicionar_estoque"),
     path('contatos/', views.contatos, name='contatos'),
-    path('extrato/', views.extrato, name='extrato')
+    path('extrato/', views.extrato, name='extrato'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
